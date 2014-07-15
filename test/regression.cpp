827694ade49b46441a0be16466ebc1932de368e3
ofenrohr <ofenrohr@tzi.de>
2014-07-15 15:08:29 +0200

    regressionstest

--- test/regression.cpp
+++ test/regression.cpp
@@ -8,39 +8,69 @@
 // generated
 #include "testboardpath.h"
 
-// kde
-#include <kDebug>
-
 class testRegression: public QObject
 {
 	Q_OBJECT
 	private slots:
 		void testBoard001();
+		void testBoard002();
 		void testBoard003();
 };
- 
+
 /**
- * Test that hard ai doesn't know about the chain rule
+ * hard ai
+ * Two short chains, one chain opened with loony move
+ * Expectation: take short chain and open second short chain with hard hearted
+ * handout
  */
 void testRegression::testBoard001()
 {
 	QScopedPointer<KSquaresGame> sGame(new KSquaresGame());
 	QList<int> lines;
 	QVERIFY(KSquaresIO::loadGame(QString(TESTBOARDPATH) + "/4x4-test1.dbl", sGame.data(), &lines));
-	QWARN(lines.toString().toLatin1().data());
+	for (int i = 0; i < lines.size(); i++)
+	{
+		sGame->addLineToIndex(lines.at(i));
+	}
 	aiController ai(-1, sGame->board()->lines(), sGame->board()->squares(), sGame->board()->width(), sGame->board()->height(), 2);
 	int aiLine = ai.chooseLine();
-	QCOMPARE( aiLine, 22 );
+	QCOMPARE( aiLine, 18 );
 }
 
 /**
- * Test that hard ai does a hard hearted handout
+ * hard ai
+ * Two short chains, one chain opened with loony move
+ * Expectation: take short chain and open second short chain with hard hearted
+ * handout
+ */
+void testRegression::testBoard002()
+{
+	QScopedPointer<KSquaresGame> sGame(new KSquaresGame());
+	QList<int> lines;
+	QVERIFY(KSquaresIO::loadGame(QString(TESTBOARDPATH) + "/4x4-test2.dbl", sGame.data(), &lines));
+	for (int i = 0; i < lines.size(); i++)
+	{
+		sGame->addLineToIndex(lines.at(i));
+	}
+	aiController ai(-1, sGame->board()->lines(), sGame->board()->squares(), sGame->board()->width(), sGame->board()->height(), 2);
+	int aiLine = ai.chooseLine();
+	QCOMPARE( aiLine, 15 );
+}
+
+/**
+ * hard aiTest that
+ * One short chain, one long chain
+ * Expectation: ai does a hard hearted handout
  */
 void testRegression::testBoard003()
 {
 	QScopedPointer<KSquaresGame> sGame(new KSquaresGame());
-	QScopedPointer< QList<int> > lines(new QList<int>());
-	QVERIFY(KSquaresIO::loadGame(QString(TESTBOARDPATH) + "/3x3-test3.dbl", sGame.data(), lines.data()));
+	QList<int> lines;
+	QVERIFY(KSquaresIO::loadGame(QString(TESTBOARDPATH) + "/3x3-test3.dbl", sGame.data(), &lines));
+	for (int i = 0; i < lines.size(); i++)
+	{
+		sGame->addLineToIndex(lines.at(i));
+	}
 	aiController ai(-1, sGame->board()->lines(), sGame->board()->squares(), sGame->board()->width(), sGame->board()->height(), 2);
 	int aiLine = ai.chooseLine();
 	QCOMPARE( aiLine, 5 );
